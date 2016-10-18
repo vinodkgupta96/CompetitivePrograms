c435189acf1fbba54a8a81d204300200849bf4ad
Vinod Kumar <vinodkgupta.1996@gmail.com>
2016-10-18 19:10:52 +0530

    OCT16 CHDOGS.cpp

---
+++ Codechef/CHDOGS.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define nl cout<<endl; 
+#define sp cout<<" ";
+#define arrsz 100000
+#define testcase int T; cin>>T; for (int p = 1; p <=T; ++p)
+#define dbg1 cout<<"#"<<endl;
+#define dbg2 cout<<"@"<<endl;
+#define gc getchar
+/*bool isPrime(int n)
+{
+    
+    if (n <= 1)  return false;
+    if (n <= 3)  return true;
+ 
+    if (n%2 == 0 || n%3 == 0) return false;
+ 
+    for (int i=5; i*i<=n; i=i+6)
+        if (n%i == 0 || n%(i+2) == 0)
+           return false;
+ 
+    return true;
+}
+*/
+int fast_read_int() {
+  char c = gc();
+  while(c<'0' || c>'9') c = gc();
+  int ret = 0;
+  while(c>='0' && c<='9') {
+    ret = 10 * ret + c - 48;
+    c = gc();
+  }
+  return ret;
+ 
+}
+ 
+ 
+int main()
+{
+ 
+    testcase 
+    {
+        double s,v; cin>>s>>v;
+        
+        double ans = 2*s/(3*v);
+        cout<<fixed<<ans; nl
+    }
+    return 0;
+}
