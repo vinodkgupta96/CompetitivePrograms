d111c501a5be09e1db35d0f97ac326f348f3bc82
Vinod Kumar <vinodkgupta.1996@gmail.com>
2016-10-18 18:38:35 +0530

    LOCJUN16 SKYSCR solution

---
+++ Codechef/SKYSCR.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{   int T; cin>>T;
+ 
+   while(T--){long long n,m;
+      cin>>n>>m;
+      (n-m>0?cout<<n-m:cout<<m-n);
+       cout<<endl; 
+       }
+       return 0;
+} 
