5e8834301f3e3c0d91733518acca0ec571b85b40
Vinod Kumar <vinodkgupta.1996@gmail.com>
2016-10-18 20:20:34 +0530

    Interviewbit find-duplicate-in-array solution

---
+++ Interviewbit/find-duplicate-in-array.cpp
@@ -0,0 +1,15 @@
+int Solution::repeatedNumber(const vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    
+    int root=sqrt(A.size()-1);
+    map<int,int>mp;
+    for(int i=1;i<A.size();i++)
+    {
+        
+        if(root*i<=A[i]<)
+        else
+    }
+}
