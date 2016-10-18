2eabfa71a25870aafcf847cadbe80b1f4564a53f
Vinod Kumar <vinodkgupta.1996@gmail.com>
2016-10-18 22:17:28 +0530

    inter

---
+++ Interviewbit/excel-col-number.cpp
@@ -0,0 +1,12 @@
+int Solution::titleToNumber(string A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+     int ans=0;    
+    for (int i = 0; i < A.length(); ++i)
+    {
+        ans=ans*26+(A[i]-'A'+1);
+    }
+    return ans;
+}
